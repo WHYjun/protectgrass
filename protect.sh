2713c27917cb9393629415ed15d60761bb0f39d1
sungkuk5420 <sungkuk5420@gmail.com>
2018-12-10 16:30:54 +0900

    change project path

--- protect.sh
+++ protect.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-date >> /home/ubuntu/project/protectgrass/history.log
+date >> /home/ubuntu/app/protectgrass/history.log
 
-cd /home/ubuntu/project/protectgrass/
+cd /home/ubuntu/app/protectgrass/
 
-/usr/bin/git add . >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git add . >> /home/ubuntu/app/protectgrass/history.log
 
-/usr/bin/git commit -m "protected grass" >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git commit -m "protected grass" >> /home/ubuntu/app/protectgrass/history.log
 
-/usr/bin/git push origin master >> /home/ubuntu/project/protectgrass/history.log
+/usr/bin/git push origin master >> /home/ubuntu/app/protectgrass/history.log
 
